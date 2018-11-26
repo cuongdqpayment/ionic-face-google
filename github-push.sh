4eadafa93ae3082462e27ce59df901a081568839
cuongdqpayment <cuongdq.payment@gmail.com>
2018-11-26 10:51:36 +0700

    cuongdq bo sung ignore'

--- github-push.sh
+++ github-push.sh
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m "cuongdq push by sh"
-#git remote add origin https://github.com/cuongdqpayment/heroku-ionic-web.git
+#git remote add origin https://github.com/cuongdqpayment/ionic-face-google.git
 git push -u origin master
